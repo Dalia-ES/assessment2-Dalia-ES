4dd829d8cf24ccf679402563ffc9b76c13016b0f
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-03-01 13:28:41 +0000

    Reshape spotify dataset using pivot_longer function, creating playlist_genre column

--- Spotify Clean Up.R
+++ Spotify Clean Up.R
@@ -7,7 +7,9 @@ library(tidyr) #Package for data tidying and reshaping
 library(dplyr) #Package for data manipulation and transformation
 
 #Read Spotify text file into a data frame labelled spotify
-spotify <- read.table("Spotify_Messy_200034287.txt", sep="\t", header=T)
+spotify <- read.table("Spotify_Messy_200034287.txt",
+                      sep="\t",
+                      header=T)
 
 #Display structure of the spotify data frame
 str(spotify)
@@ -17,3 +19,8 @@ View(spotify)
 
 #Print all column names of the spotify data frame
 colnames(spotify)[1:ncol(spotify)]
+
+#Reshape spotify dataset using pivot_longer, creating a new column called playlist_genre
+spotify <- spotify %>% pivot_longer(cols = c("pop","rap","rock","r.b","edm"),
+             names_to="playlist_genre")
+spotify
