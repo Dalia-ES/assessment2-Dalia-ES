067bffd5c6fa6ad6f95a1ad0b886e35bc8c69e3f
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-03-01 13:17:55 +0000

    Read and view structure of Spotify dataset

--- Spotify Clean Up.R
+++ Spotify Clean Up.R
@@ -5,3 +5,15 @@ install.packages("dplyr")
 #Load tidyverse packages into R session
 library(tidyr) #Package for data tidying and reshaping
 library(dplyr) #Package for data manipulation and transformation
+
+#Read Spotify text file into a data frame labelled spotify
+spotify <- read.table("Spotify_Messy_200034287.txt", sep="\t", header=T)
+
+#Display structure of the spotify data frame
+str(spotify)
+
+#View the spotify data frame in a table
+View(spotify)
+
+#Print all column names of the spotify data frame
+colnames(spotify)[1:ncol(spotify)]
