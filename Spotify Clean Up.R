5825ed3371e2da3c568c85407fafbdb5f01e0df4
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-03-01 13:15:23 +0000

    Install and load tidyverse packages

---
+++ Spotify Clean Up.R
@@ -0,0 +1,7 @@
+#Install tidyverse packages for data manipulation
+install.packages("tidyr")
+install.packages("dplyr")
+
+#Load tidyverse packages into R session
+library(tidyr) #Package for data tidying and reshaping
+library(dplyr) #Package for data manipulation and transformation
